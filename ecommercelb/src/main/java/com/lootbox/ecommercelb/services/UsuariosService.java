f46384fa0911b695a4dd9c909e207b0388a99b96
Felipe Rosas <felipe.rosas1199@gmail.com>
2025-05-20 13:35:30 -0600

    Se agrego el controller y sus service para poder ser utilizados

---
+++ ecommercelb/src/main/java/com/lootbox/ecommercelb/services/UsuariosService.java
@@ -0,0 +1,64 @@
+package com.lootbox.ecommercelb.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lootbox.ecommercelb.models.Usuarios;
+
+@Service
+public class UsuariosService {
+	private final List<Usuarios> lista = new ArrayList<Usuarios>();
+	@Autowired
+	public UsuariosService() {
+		//String nombre, String descripcion, String imagen, Double precio
+		lista.add(new Usuarios("Jazmin Bethzabe Robles Macias", "popotes@gmial.com", "3325201943","popotes1234"));
+		lista.add(new Usuarios("Carlos Eduardo Sánchez Pérez", "carlossanchez@gmail.com", "5512345678", "carlos2025"));
+		lista.add(new Usuarios("Luis Alberto Torres Ramírez", "luis.torres@yahoo.com", "3399887766", "luistorres99"));
+	}//constructor
+	public List<Usuarios> getUsuarios() {
+		return lista;
+	}//getUsuarios
+	public Usuarios getUsuario(Long id) {
+		Usuarios tmp = null;
+	for (Usuarios usuario : lista) {
+		if(id == usuario.getIdUsuario()) {
+			tmp = usuario;
+			break;
+		}//if
+	}//forEach		
+	return tmp;
+	}//getUsuario
+	public Usuarios addUsuario(Usuarios usuario) {
+		lista.add(usuario);
+		return usuario;
+	}//addUsuario
+	public Usuarios deletUsuario(Long id) {
+		Usuarios tmp = null;
+		for (Usuarios usuario : lista) {
+			if(id == usuario.getIdUsuario()) {
+				tmp = usuario;
+				lista.remove(usuario);
+				break;
+			}//if
+		}//forEach		
+		return tmp;
+	}//DeletUsuario
+	public Usuarios updateUsuario(Long id, String nombre, String email, String telefono, String contraseña) {
+		Usuarios tmp = null;
+		for (Usuarios usuario : lista) {
+			if(id == usuario.getIdUsuario()) {
+				if(nombre!=null)usuario.setNombre(nombre);
+				if(email!=null)usuario.setEmail(email);
+				if(telefono!=null)usuario.setTelefono(telefono);
+				if(contraseña!=null)usuario.setContraseña(contraseña);
+				tmp=  usuario;
+				break;
+			}//if
+		}//forEach
+		return tmp;
+	}//updateUsuario
+}//class UsuarioService
